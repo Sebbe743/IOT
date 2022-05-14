e53ee7b276173a9fe704539fcdbde0d9ecca36ee
Sebbe <12002498@student.pxl.be>
2022-05-14 11:43:10 +0200

    styling, submitting file keep style sheet. no isset error

--- DataHandling.php
+++ DataHandling.php
@@ -1,21 +1,49 @@
-<?php
-	   $_age = "";
-	   $_name = "";
-   if( $_GET["name"] || $_GET["age"] ) {
-      echo "Welcome ". $_GET['name']. "<br />";
-      echo "You are ". $_GET['age']. " years old.";
-      
-      exit();
-   }
-?>
 <html>
-   <body>
-   
-      <form action = "<?php $_PHP_SELF ?>" method = "GET">
-         Name: <input type = "text" name = "name" />
-         Age: <input type = "text" name = "age" />
-         <input type = "submit" />
-      </form>
-      
-   </body>
+	<head>
+		<link rel="stylesheet" href="Originality.css">
+		<title> DataHandling </title>	
+	</head>
+
+		<?php
+			
+			$name = "";
+			$age = "";
+			
+			if(isset($_GET['name']))
+			{
+				$name = $_GET['name'];
+			}
+			
+			if(isset($_GET['age']))
+			{
+				$age = $_GET['age'];
+			}
+			
+			if( $name!="" &&  $age != "") 
+			{
+			  echo "Welcome ". $name . "<br />";
+			  echo "You are ". $age . " years old.";
+			  
+			  exit();
+			}
+		   else
+		   {
+			echo "Please enter an age and name";
+		   }
+		?>
+	<body>
+		<table width= "100%">
+			<tr>
+			<td width= "33%" ><a href= "index.html"> Homepage </a></td>
+			<td width= "33%"><a href = "TempCheck.PHP"> Tables </a></td>
+			<td width= "33%" ><a href= "DataHandling.php"> Datahandling </a></td>
+			</tr>
+		</table>
+
+		<form action = "<?php $_PHP_SELF ?>" method = "GET">
+			Name: <input type = "text" name = "name" />		
+			Age: <input type = "text" name = "age" />
+			<input type = "submit" />
+		</form>
+	</body>
 </html>

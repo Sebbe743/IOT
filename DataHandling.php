392ecb1b61f659aea41e94333724f04cc1807ade
Sebbe <12002498@student.pxl.be>
2022-08-04 13:50:28 +0200

    Visual table change. Fixing data handling

--- DataHandling.php
+++ DataHandling.php
@@ -1,3 +1,4 @@
+
 <html>
 	<head>
 		<link rel="stylesheet" href="Originality.css">
@@ -11,55 +12,42 @@
 			<td width= "33%" ><a href= "DataHandling.php"> <h1> Datahandling </h1> </a> </td>
 			</tr>
 		</table>
-		<?php
-			
-			$type = "";
-			$value = "";
-			
-			$ID =01;
-			$val =22.1;
-			include 'Connection.PHP';
-			
-			$sql = "INSERT INTO `Values` (`ID-Sensor`, `Waarde`, `Tijdstip`)
-			VALUES ($ID, $val, date('Y-m-d H:i:s'))";
-			if(mysqli_query($conn, $sql))
-			{
-				echo "<br />data added";
-				sleep(2);
-			}
-			else
-			{
-				echo "error";
-			}
-			
-			if(isset($_GET['type']))
-			{
-				$type = $_GET['type'];
-			}
-			
-			if(isset($_GET['value']))
-			{
-				$value = $_GET['value'];
-			}
-			
-			if( $type!="" &&  $value != "") 
-			{
-			  echo "data sent, type". $type ."<br />";
-			  echo "value ". $value  . "<br />";
-			  exit();
-			}
-		   else
-		   {
-			echo "<br />Please enter a value and type <br />";
-		   }
-		
-		?>
+<?php
+	include 'Connection.PHP';
+	include 'RandomNumberGen.PHP';
+	
+	$ID = $SensorID;
+	$val = 0;
+	//$val = $_POST['value'];
+	
+	if(isset($_POST['Sub']))
+	{
+		$val = $_POST['value'];
+		$sql = "INSERT INTO `Values` (`ID-Sensor`, `Waarde`, `Tijdstip`)
+		VALUES ($ID, $val, date('Y-m-d H:i:s'))";
+		if(mysqli_query($conn, $sql))
+		{
+			echo "<br />data added";
+		}
+		else
+		{
+			echo "error";
+		}			
+	}
+	if($val != "") 
+	{
+	  echo "<br /> value is ". $val  . "<br />";
+	}
+	else
+	{
+		echo "<br />Please enter a value<br />";
+	}
+?>
 	
 
-		<form action = "<?php $_PHP_SELF ?>" method = "GET">
-			type: <input type = "text" type = "type" />		
-			value: <input type = "text" type = "value" />
-			<input type = "submit" />
+		<form action = "#" method = "post">
+			value: <input type = "text" name = "value" />
+			<input type = "submit" name="Sub" />
 		</form>
 	</body>
 </html>
